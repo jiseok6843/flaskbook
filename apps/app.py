28846bfa6b46d27082a6082d90233274f24383da
jiseok6843 <jiseok6843@gmail.com>
2023-12-15 15:29:40 +0900

    23.12.15까지의 진도

--- apps/app.py
+++ apps/app.py
@@ -1,18 +1,33 @@
+from pathlib import Path
 from flask import Flask
-from flask import Blueprint, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-crud = Blueprint("crud",__name__, template_folder="template", static_folder="static")
+db = SQLAlchemy()
 
-@crud.route("/")
-def index():
-    return render_template("crud/index.html")
-
-
+#create_app함수 생성
 def create_app():
-    app=Flask(__name__)
+
+    # 플라스크 인스턴스(객체)를 생성
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SECRET_KEY="googleCloudPlatform",
+        SQLALCHEMY_DATABASE_URI=
+        f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_ECHO=True, 
+        #WTF_CSRF_SECRET_KEY="sadasdsdasde32324dfg",
+    )
+
+    db.init_app(app)
+    Migrate(app, db)
+
 
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
     return app
+
+
